c7a486153fb0720fdf5281bf07fa01a1fe64af25
Любаев Павел Александрович <p.lyubaev@sigma-it.ru>
2022-10-14 15:52:36 +0500

    delete some files

--- kubernetes-volumes/run.sh
+++
@@ -1,5 +0,0 @@
-./kind delete cluster
-./kind create cluster --config kind-config.yaml
-./kubectl apply -f minio-statefulset.yaml
-./kubectl apply -f minio-headless-service.yaml
-./kubectl apply -f minio-secrets.yaml
